7f59a3a9beaf036af6e0734e15db90605fc4ba9c
Marshall Hotaling <marshall.hotaling@gmail.com>
2024-09-23 11:04:54 -0600

    finished ittoration 2 abd made the alert for the inputs for ittoration 3

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -1,18 +1,41 @@
-import { useState } from "react";
+import {useEffect, useState} from "react";
 
 function OrderForm(props) {
   const [name, setName] = useState("");
   const [ingredients, setIngredients] = useState([]);
 
+  useEffect(() => {
+
+  }, [ingredients]);
+
   function handleSubmit(e) {
-    e.preventDefault();
-    clearInputs();
+    if (name !== '' && ingredients.length > 0) {
+      console.log('setName', name, 'ing', ingredients)
+
+
+      clearInputs();
+    } else {
+      alert('You need a name and at least one ingredient')
+    }
   }
 
   function clearInputs() {
     setName("");
     setIngredients([]);
   };
+
+  function saveName() {
+    setName(document.getElementById("nameInput").value)
+  };
+
+  function saveIngredients(item) {
+    const holder = [];
+    holder.push(ingredients)
+    holder.push(item)
+    setIngredients(holder)
+    console.log('ingredients', ingredients)
+  }
+
 
   const possibleIngredients = [
     "beans",
@@ -30,32 +53,41 @@ function OrderForm(props) {
   ];
   const ingredientButtons = possibleIngredients.map((ingredient) => {
     return (
-      <button
-        key={ingredient}
-        name={ingredient}
-        onClick={(e) => console.log('')}
-      >
-        {ingredient}
-      </button>
+        <button
+            key={ingredient}
+            name={ingredient}
+            onClick={(e) => {
+              e.preventDefault()
+              saveIngredients(ingredient)
+            }}
+        >
+          {ingredient}
+        </button>
     );
   });
 
   return (
-    <form>
-      <input
-        type="text"
-        placeholder="Name"
-        name="name"
-        value={name}
-        onChange={(e) => console.log('')}
-      />
+      <form>
+        <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            id="nameInput"
+            onChange={(e) => {
+              e.preventDefault()
+              saveName()
+            } }/>
 
-      {ingredientButtons}
+        {ingredientButtons}
 
-      <p>Order: {ingredients.join(", ") || "Nothing selected"}</p>
+        <p>Order: {ingredients.join(", ") || "Nothing selected"}</p>
 
-      <button onClick={(e) => handleSubmit(e)}>Submit Order</button>
-    </form>
+        <button onClick={(e) => {
+          e.preventDefault()
+          handleSubmit(e)
+        }}>Submit Order</button>
+      </form>
   );
 }
 
